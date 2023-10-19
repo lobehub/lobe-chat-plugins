83997625b0002f1d844dbc18c995625d2a4af8da
CanisMinor <i@canisminor.cc>
2023-10-19 15:04:37 +0800

    🔧 chore: .i18nrc.js

--- .i18nrc.js
+++ .i18nrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   selectors: ['meta.title', 'meta.description', 'meta.tags'],
   entryLocale: 'en-US',
-  outputLocales: ['zh-CN', 'ru-RU'],
-  // outputLocales: ['zh-CN', 'zh-TW', 'ru-RU', 'ja-JP', 'ko-KR'],
+  outputLocales: ['zh-CN', 'zh-TW', 'ru-RU', 'ja-JP', 'ko-KR'],
 };
