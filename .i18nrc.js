1fa1be69d2eb72ca520baa06fa2b249decf94f3d
CanisMinor <i@canisminor.cc>
2023-12-16 00:28:34 +0800

    Update .i18nrc.js

--- .i18nrc.js
+++ .i18nrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   selectors: ['meta.title', 'meta.description', 'meta.tags'],
   entryLocale: 'en-US',
-  outputLocales: ['zh-CN', 'zh-TW', 'ru-RU', 'ja-JP', 'ko-KR'],
+  outputLocales: ['zh-CN', 'zh_TW', 'ru_RU', 'ja_JP', 'ko_KR', 'fr_FR'],
 };
