2959cf38389943869dc8e3e8ee81be8ef4cb91e4
CanisMinor <i@canisminor.cc>
2023-12-16 00:29:14 +0800

    Update .i18nrc.js

--- .i18nrc.js
+++ .i18nrc.js
@@ -2,4 +2,5 @@ module.exports = {
   selectors: ['meta.title', 'meta.description', 'meta.tags'],
   entryLocale: 'en-US',
   outputLocales: ['zh-CN', 'zh_TW', 'ru_RU', 'ja_JP', 'ko_KR', 'fr_FR'],
+  modelName: 'gpt-3.5-turbo-1106',
 };
